1901de70ce4e88d28aaffcca422fe95b25a98aca
Ahoo Wang <ahoowang@qq.com>
2021-06-28 14:00:54 +0800

    clean import

--- cosid-core/src/main/java/me/ahoo/cosid/snowflake/ClockSyncSnowflakeId.java
+++ cosid-core/src/main/java/me/ahoo/cosid/snowflake/ClockSyncSnowflakeId.java
@@ -1,7 +1,6 @@
 package me.ahoo.cosid.snowflake;
 
 import lombok.extern.slf4j.Slf4j;
-import me.ahoo.cosid.CosIdException;
 import me.ahoo.cosid.IdGenerator;
 import me.ahoo.cosid.snowflake.exception.ClockBackwardsException;
 

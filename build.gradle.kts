862748479035a11cf822273809e498d342eeb924
Ahoo Wang <ahoowang@qq.com>
2022-01-14 09:17:36 +0800

    fix CI(codeql-analysis)

--- build.gradle.kts
+++ build.gradle.kts
@@ -152,8 +152,8 @@ configure(publishProjects) {
                 name = "GitHubPackages"
                 url = uri("https://maven.pkg.github.com/Ahoo-Wang/CosId")
                 credentials {
-                    username = project.findProperty("gitHubPackagesUserName") as String
-                    password = project.findProperty("gitHubPackagesToken") as String
+                    username = project.findProperty("gitHubPackagesUserName") as String?
+                    password = project.findProperty("gitHubPackagesToken") as String?
                 }
             }
         }

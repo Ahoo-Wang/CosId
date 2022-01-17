82cdc3b7290d63adafcb9175f2afcb712903e9d0
Ahoo Wang <ahoowang@qq.com>
2022-01-17 09:16:54 +0800

    Upgrade to Guava 30.0-jre (https://github.com/Ahoo-Wang/CosId/issues/13)

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ val libraryProjects = publishProjects - bomProjects
 
 ext {
     set("lombokVersion", "1.18.20")
-    set("guavaVersion", "29.0-jre")
+    set("guavaVersion", "30.0-jre")
     set("springBootVersion", "2.4.13")
     set("springCloudVersion", "2020.0.5")
     set("springfoxVersion", "3.0.0")

3c804f738caacfa2c08e0af6608287a0dd03bb2e
Ahoo Wang <ahoowang@qq.com>
2022-04-13 22:15:53 +0800

    Improve code coverage: `ManualMachineIdDistributor`

---
+++ cosid-core/src/test/java/me/ahoo/cosid/snowflake/machine/ManualMachineIdDistributorTest.java
@@ -0,0 +1,38 @@
+package me.ahoo.cosid.snowflake.machine;
+
+import me.ahoo.cosid.snowflake.ClockBackwardsSynchronizer;
+import me.ahoo.cosid.util.MockIdGenerator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * ManualMachineIdDistributorTest .
+ *
+ * @author ahoo wang
+ */
+class ManualMachineIdDistributorTest {
+    public static final int TEST_MANUAL_MACHINE_ID = 1;
+    ManualMachineIdDistributor machineIdDistributor;
+    
+    @BeforeEach
+    void setup() {
+        machineIdDistributor = new ManualMachineIdDistributor(TEST_MANUAL_MACHINE_ID, MachineStateStorage.LOCAL, ClockBackwardsSynchronizer.DEFAULT);
+    }
+    
+    @Test
+    void getMachineId() {
+        Assertions.assertEquals(TEST_MANUAL_MACHINE_ID, machineIdDistributor.getMachineId());
+    }
+    
+    @Test
+    void distribute() {
+        Assertions.assertEquals(TEST_MANUAL_MACHINE_ID, machineIdDistributor.distribute(MockIdGenerator.INSTANCE.generateAsString(), InstanceId.NONE));
+    }
+    
+    @Test
+    void revert() {
+        machineIdDistributor.revert(MockIdGenerator.INSTANCE.generateAsString(), InstanceId.NONE);
+    }
+}

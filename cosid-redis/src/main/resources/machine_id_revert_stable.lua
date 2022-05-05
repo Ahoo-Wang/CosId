8f83868e3d74b8eae30026620528122afd85f0db
RocherKong <rocher@126.com>
2022-05-05 09:47:02 +0800

    refactor redis script

--- cosid-redis/src/main/resources/machine_id_revert_stable.lua
+++ cosid-redis/src/main/resources/machine_id_revert_stable.lua
@@ -5,22 +5,22 @@ local lastStamp = ARGV[2];
 
 local instanceIdxKey = 'cosid' .. ':' .. namespace .. ':itc_idx';
 
-local function convertStateFromString(machineState)
+local function convertStingToState(machineState)
     local splitIdx = string.find(machineState, stateDelimiter, 1);
     local machineId = string.sub(machineState, 1, splitIdx - 1);
-    local stamp = string.sub(machineState, splitIdx + 1, -1);
-    return { machineId, stamp }
+    local timestamp = string.sub(machineState, splitIdx + 1, -1);
+    return { tonumber(machineId), tonumber(timestamp) }
 end
 
-local function convertToStringState(machineId, lastStamp)
-    return tostring(machineId) .. stateDelimiter .. tostring(lastStamp);
+local function convertStateToString(machineId, timestamp)
+    return tostring(machineId) .. stateDelimiter .. tostring(timestamp);
 end
 
 local machineState = redis.call('hget', instanceIdxKey, instanceId)
 if machineState then
-    local states = convertStateFromString(machineState)
+    local states = convertStingToState(machineState)
     local machineId = states[1];
-    machineState = convertToStringState(machineId, lastStamp);
+    machineState = convertStateToString(machineId, lastStamp);
     redis.call('hset', instanceIdxKey, instanceId, machineState);
     return 1;
 end

0f27e1e5a65efc8da69b4f7f9bdb78c785e525c3
Rocher <rocher@126.com>
2022-05-08 18:32:38 +0800

    add unit-test for SpringRedisIdSegmentDistributor (#85)
    * fix demo not runnable by correct application.yaml
    
    * ignore type check while read integer field.
    
    * update CosIdIntervalShardingAlgorithm type name.
    
    * add test for idConverter. and add some docs.
    
    * add IdConverter-impl-class diagram.
    
    * add DefaultIdGeneratorProvider Set Test.
    
    * add ExactCollectionTest.
    
    * add CosId and CosIdExceptionTest.
    
    * add test for CosId Mod Sharding Algorithm getProps and setProps.
    
    * add test for CosId Mod Sharding Algorithm.
    
    * add test for CosId Mod Sharding Algorithm.
    
    * add test for SnowflakeFriendlyIdConverterTest getParser
    
    * add test for NotFoundIdGeneratorExceptionTest
    
    * add test for IdSegmentDistributorDefinitionTest
    
    * add test for MergedIdSegmentTest
    
    * add test for NextIdSegmentExpiredExceptionTest
    
    * add test for IntervalStepTest
    
    * add test for ClockSyncSnowflakeId
    
    * add NotSupportIntervalShardingTypeExceptionTest
    
    * add test for NotSupportModShardingTypeException and CosIdKeyGenerateAlgorithm
    
    * SpringRedisIdSegmentDistributorTest

--- cosid-spring-redis/src/test/java/me/ahoo/cosid/spring/redis/SpringRedisIdSegmentDistributorTest.java
+++ cosid-spring-redis/src/test/java/me/ahoo/cosid/spring/redis/SpringRedisIdSegmentDistributorTest.java
@@ -14,10 +14,15 @@
 package me.ahoo.cosid.spring.redis;
 
 import me.ahoo.cosid.segment.IdSegmentDistributor;
+import me.ahoo.cosid.segment.IdSegmentDistributorDefinition;
 import me.ahoo.cosid.segment.IdSegmentDistributorFactory;
+import me.ahoo.cosid.test.Assert;
+import me.ahoo.cosid.test.MockIdGenerator;
 import me.ahoo.cosid.test.segment.distributor.IdSegmentDistributorSpec;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -28,7 +33,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 class SpringRedisIdSegmentDistributorTest extends IdSegmentDistributorSpec {
     StringRedisTemplate stringRedisTemplate;
     SpringRedisIdSegmentDistributorFactory distributorFactory;
-    
+    protected IdSegmentDistributorDefinition idSegmentDistributorDefinition;
+
     @BeforeEach
     private void setup() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
@@ -36,17 +42,38 @@ class SpringRedisIdSegmentDistributorTest extends IdSegmentDistributorSpec {
         lettuceConnectionFactory.afterPropertiesSet();
         stringRedisTemplate = new StringRedisTemplate(lettuceConnectionFactory);
         distributorFactory = new SpringRedisIdSegmentDistributorFactory(stringRedisTemplate);
+        idSegmentDistributorDefinition = new IdSegmentDistributorDefinition("RedisIdSegmentDistributorFactoryTest", MockIdGenerator.INSTANCE.generateAsString(), 0, 100);
     }
-    
-    
+
+
     @Override
     protected IdSegmentDistributorFactory getFactory() {
         return distributorFactory;
     }
-    
+
     @Override
     protected <T extends IdSegmentDistributor> void setMaxIdBack(T distributor, long maxId) {
-        String adderKey = ((SpringRedisIdSegmentDistributor)distributor).getAdderKey();
+        String adderKey = ((SpringRedisIdSegmentDistributor) distributor).getAdderKey();
         stringRedisTemplate.opsForValue().set(adderKey, String.valueOf(maxId - 1));
+    }
+
+
+    @Test
+    protected void distributorFactoryTest() {
+        IdSegmentDistributor idSegmentDistributor = distributorFactory.create(idSegmentDistributorDefinition);
+        Assertions.assertNotNull(idSegmentDistributor);
+    }
+
+    @Test
+    void getAdderKey() {
+        IdSegmentDistributor idSegmentDistributor = distributorFactory.create(idSegmentDistributorDefinition);
+        Assertions.assertTrue(idSegmentDistributor instanceof SpringRedisIdSegmentDistributor);
+        SpringRedisIdSegmentDistributor springRedisIdSegmentDistributor=(SpringRedisIdSegmentDistributor)idSegmentDistributor;
+        Assertions.assertNotNull(springRedisIdSegmentDistributor.getAdderKey());
+        Assertions.assertNotNull(springRedisIdSegmentDistributor.getName());
+        Assertions.assertTrue(springRedisIdSegmentDistributor.getOffset()==0);
+        Assertions.assertTrue(springRedisIdSegmentDistributor.getStep()==100);
+        Assertions.assertNotNull(springRedisIdSegmentDistributor.getNamespace());
+        Assertions.assertNotNull(springRedisIdSegmentDistributor.nextMaxId(100));
     }
 }

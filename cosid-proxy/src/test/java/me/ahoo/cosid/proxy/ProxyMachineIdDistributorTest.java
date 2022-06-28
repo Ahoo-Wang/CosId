44fa9c66b6d839c501d812475b3156fe3abd07d3
Ahoo Wang <ahoowang@qq.com>
2022-06-28 11:37:18 +0800

    * Support CI for CosId-Proxy * Update dependency `Spring Boot` to `v2.6.9` * Update dependency `Spring Cloud` to `2021.0.3` * Update dependency `spotbugs-gradle-plugin` to `5.0.9`

--- cosid-proxy/src/test/java/me/ahoo/cosid/proxy/ProxyMachineIdDistributorTest.java
+++ cosid-proxy/src/test/java/me/ahoo/cosid/proxy/ProxyMachineIdDistributorTest.java
@@ -19,15 +19,12 @@ import me.ahoo.cosid.snowflake.machine.MachineStateStorage;
 import me.ahoo.cosid.test.snowflake.machine.distributor.MachineIdDistributorSpec;
 
 import okhttp3.OkHttpClient;
-import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
-@DisabledIfEnvironmentVariable(named = "CI", matches = "true")
 class ProxyMachineIdDistributorTest extends MachineIdDistributorSpec {
-    public final static String PROXY_HOST = "http://localhost:8688";
     
     @Override
     protected MachineIdDistributor getDistributor() {
-        return new ProxyMachineIdDistributor(new OkHttpClient(), PROXY_HOST, MachineStateStorage.LOCAL, ClockBackwardsSynchronizer.DEFAULT);
+        return new ProxyMachineIdDistributor(new OkHttpClient(), ProxyServerLauncher.COSID_PROXY_HOST, MachineStateStorage.LOCAL, ClockBackwardsSynchronizer.DEFAULT);
     }
     
     @Override

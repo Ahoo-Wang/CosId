b78839b5d0efa6738f63be7ebc7f6a9cd6eb1d98
Ahoo Wang <ahoowang@qq.com>
2023-03-17 20:47:45 +0800

    support PadStartIdConverter for `ToString`. #285

--- cosid-spring-boot-starter/src/main/java/me/ahoo/cosid/spring/boot/starter/snowflake/CosIdSnowflakeAutoConfiguration.java
+++ cosid-spring-boot-starter/src/main/java/me/ahoo/cosid/spring/boot/starter/snowflake/CosIdSnowflakeAutoConfiguration.java
@@ -97,6 +97,10 @@ public class CosIdSnowflakeAutoConfiguration {
         IdConverter idConverter = ToStringIdConverter.INSTANCE;
         switch (converterDefinition.getType()) {
             case TO_STRING: {
+                IdConverterDefinition.ToString toString = converterDefinition.getToString();
+                if (toString != null) {
+                    idConverter = new ToStringIdConverter(toString.isPadStart(), toString.getCharSize());
+                }
                 break;
             }
             case SNOWFLAKE_FRIENDLY: {

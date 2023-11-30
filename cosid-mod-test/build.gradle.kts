33da18259ab971dc5304b50f3387dbd0fa9b49f5
Ahoo Wang <ahoowang@qq.com>
2023-11-30 14:42:53 +0800

    feat: add `ModSpec` to support Modulo sharding algorithm uniformity testing

--- cosid-mod-test/build.gradle.kts
+++ cosid-mod-test/build.gradle.kts
@@ -10,7 +10,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+plugins {
+    java
+}
 dependencies {
     implementation(project(":cosid-core"))
     testImplementation(project(":cosid-test"))

b406e4a023bb4d1ea7c97d8b3fc35d8b0c6cd1c0
Ahoo Wang <ahoowang@qq.com>
2025-01-15 14:19:26 +0800

    feat(doc): update doc style (#700)

---
+++ documentation/docs/.vitepress/theme/index.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import DefaultTheme from 'vitepress/theme'
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
+import mediumZoom from 'medium-zoom'
+import './global.css'
+
+export default {
+    ...DefaultTheme,
+    setup() {
+        const route = useRoute()
+        const initZoom = () => {
+            mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
+        }
+        onMounted(() => {
+            initZoom()
+        })
+        watch(
+            () => route.path,
+            () => nextTick(() => initZoom())
+        )
+    },
+}

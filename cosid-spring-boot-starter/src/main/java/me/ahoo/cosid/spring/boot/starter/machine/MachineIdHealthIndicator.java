f2ff07d621a5af9ed52aa367140c84e11a178b18
Ahoo Wang <ahoowang@qq.com>
2025-11-24 22:28:48 +0800

    feat(machine): add machine id health indicator for spring boot actuator
    - Add MachineIdHealthIndicator to monitor machine id guarder status
    - Configure health endpoint to show details always in redis example
    - Enable health check integration with spring boot actuator
    - Provide health status reporting for cosid machine lifecycle management

---
+++ cosid-spring-boot-starter/src/main/java/me/ahoo/cosid/spring/boot/starter/machine/MachineIdHealthIndicator.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.ahoo.cosid.spring.boot.starter.machine;
+
+import me.ahoo.cosid.machine.MachineIdGuarder;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+
+public class MachineIdHealthIndicator implements HealthIndicator {
+    private final MachineIdGuarder machineIdGuarder;
+
+    /**
+     * Constructs a MachineHealthIndicator with the given MachineIdGuarder.
+     *
+     * @param machineIdGuarder the machine ID guarder to monitor
+     */
+    public MachineIdHealthIndicator(MachineIdGuarder machineIdGuarder) {
+        this.machineIdGuarder = machineIdGuarder;
+    }
+
+    @Override
+    public Health health() {
+        boolean hasFailure = machineIdGuarder.hasFailure();
+        if (hasFailure) {
+            return Health.down()
+                .build();
+        }
+        return Health.up().build();
+    }
+}

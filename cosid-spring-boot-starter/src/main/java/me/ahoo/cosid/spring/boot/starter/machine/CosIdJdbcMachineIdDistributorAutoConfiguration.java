f07894c706c60a3dcea48f99a4caa985aca3caad
Ahoo Wang <ahoowang@qq.com>
2026-07-07 15:58:45 +0800

    style: remove unused Configuration import
    The class is annotated with @AutoConfiguration; the
    org.springframework.context.annotation.Configuration import was unused
    (inherited from the pre-existing file). Aligns with the sibling
    CosIdJdbcSegmentAutoConfiguration which does not import it.

--- cosid-spring-boot-starter/src/main/java/me/ahoo/cosid/spring/boot/starter/machine/CosIdJdbcMachineIdDistributorAutoConfiguration.java
+++ cosid-spring-boot-starter/src/main/java/me/ahoo/cosid/spring/boot/starter/machine/CosIdJdbcMachineIdDistributorAutoConfiguration.java
@@ -27,7 +27,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
